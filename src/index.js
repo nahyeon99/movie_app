9c5dfde4db5e0e0eba060b6c795376197fc220da
NaHyeon <nahyeonee99@gmail.com>
2021-04-17 19:33:10 +0900

    #2 JSX & PROPS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import propTypes from "prop-types";
+
+function Coffee({ name, rating, picture }) {
+  return (
+    <div>
+      <h3>I love {name}</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const coffeeILike = [
+  {
+    name: "starbucks",
+    rating: 5,
+    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
+  },
+  {
+    name: "hollys",
+    rating: 4,
+    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
+  },
+  {
+    name: "twosome",
+    rating: 3,
+    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
+  },
+  {
+    name: "coffee bean",
+    rating: 3,
+    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
+  },
+];
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {coffeeILike.map((cafe) => (
+      <Coffee name={cafe.name} rating={cafe.rating} picture={cafe.img} />
+    ))}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Coffee.propTypes = {
+  name: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+  picture: propTypes.string.isRequired,
+};
