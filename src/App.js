d9eca8a5b1aaa7cc5d21310dfce87d05df5eae54
NaHyeon <nahyeonee99@gmail.com>
2021-04-17 19:53:55 +0900

    #3 STATE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "I'm ready"}</div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import propTypes from "prop-types";
-
-function Coffee({ name, rating, picture }) {
-  return (
-    <div>
-      <h3>I love {name}</h3>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const coffeeILike = [
-  {
-    name: "starbucks",
-    rating: 5,
-    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
-  },
-  {
-    name: "hollys",
-    rating: 4,
-    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
-  },
-  {
-    name: "twosome",
-    rating: 3,
-    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
-  },
-  {
-    name: "coffee bean",
-    rating: 3,
-    img: "https://cdn.imweb.me/thumbnail/20201002/0f846113b6bee.jpg",
-  },
-];
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {coffeeILike.map((cafe) => (
-      <Coffee name={cafe.name} rating={cafe.rating} picture={cafe.img} />
-    ))}
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-Coffee.propTypes = {
-  name: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired,
-  picture: propTypes.string.isRequired,
-};
